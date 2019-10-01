6045a57a06544ef59b42b15baf4919621f82fe3b
JosemaRguez <rguezjosemanuel@gmail.com>
2019-10-01 10:47:55 +0200

    For loop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatCardModule} from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Dashboard } from './dashboard/dashboard.component';
@@ -11,6 +12,7 @@ import { Dashboard } from './dashboard/dashboard.component';
     Dashboard
   ],
   imports: [
+    MatCardModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+
 
 @Component({
     selector: 'dashboard',
@@ -7,6 +8,63 @@ import { Component } from '@angular/core'
 })
 
 export class Dashboard {
+    constructor() { }
+
+    ngOnInit() {
+        this.divideTasks()
+    }
+
     public name = 'Dashboard';
+    public dashboardTitles = [
+        'Pendiente',
+        'En progreso',
+        'Terminado',
+        'Revisado',
+    ];
+    public tasks = [
+        {
+            'description': 'Automatizar tareas',
+            'state': 0
+        },
+         {
+            'description': 'Crear una lista',
+            'state': 1
+        },
+        {
+            'description': 'Ejectuar npm start',
+            'state': 1
+        },
+         {
+            'description': 'Crear proyecto',
+            'state': 3
+        }
+    ];
+
+    taskStatusSplit = { 
+        '0': [],
+        '1': [],
+        '2': [],
+        '3': [],
+    };
+
+
+    divideTasks = () => {
+        this.tasks.map((task) => {
+            if(task.state === 0){
+                this.taskStatusSplit['0'].push(task.description)
+            }
+            if(task.state === 1){
+                this.taskStatusSplit['1'].push(task.description)
+            }
+            if(task.state === 2){
+                this.taskStatusSplit['2'].push(task.description)
+            }
+            if(task.state === 3){
+                this.taskStatusSplit['3'].push(task.description)
+            }
+        })
+    }
   }
   
+
+

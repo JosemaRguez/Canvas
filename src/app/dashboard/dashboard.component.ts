e94cd025ee0b3e8c4a2f9b7d7a02540e3ad4720e
JosemaRguez <rguezjosemanuel@gmail.com>
2019-10-01 15:13:12 +0200

    Removing tasks. Identifiers added.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,27 +20,38 @@ export class Dashboard {
         'Terminado',
         'Revisado',
     ];
+    public identificators = 4
     public taskStatusSplit = {
         '0': [{
+            'id': 1,
             'description': 'Automatizar tareas'
         }],
         '1': [{
+            'id': 2,
             'description': 'Crear una lista'
         },
         {
+            'id': 3,
             'description': 'Ejectuar npm start'
         }],
         '2': [],
         '3': [
             {
+                'id': 4,
                 'description': 'Crear proyecto'
             }
         ]
     };
 
     handleSubmitForm = (description, state) => {
-        this.taskStatusSplit[state].push({ 'description': description})
-        console.log(this.taskStatusSplit)
+        if(description && description.length > 0  ){
+            this.identificators++
+            this.taskStatusSplit[state].push({ 'description': description, 'id': this.identificators})
+        }
+    }
+
+    handleDelete = (id) => {
+        console.log(id)
     }
 }
 
